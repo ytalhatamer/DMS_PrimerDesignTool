40cee7401333fbeb0bcb8a5e05823836e599b3fb
ytamer <yusuf.tamer@utsouthwestern.edu>
2020-07-14 15:55:36 -0500

    All global variables are added.

--- globalvars.py
+++ globalvars.py
@@ -1,3 +1,4 @@
+# Global codon translation table
 translation={'ACC':'T','ATG':'M','AAG':'K','AAA':'K','ATC':'I','AAC':'N','ATA':'I','AGG':'R',
              'CCT':'P','CTC':'L','AGC':'S','ACA':'T','AGA':'R','CAT':'H','AAT':'N','ATT':'I',
              'CTG':'L','CTA':'L','ACT':'T','CAC':'H','ACG':'T','CAA':'Q','AGT':'S','CAG':'Q',
@@ -7,21 +8,26 @@ translation={'ACC':'T','ATG':'M','AAG':'K','AAA':'K','ATC':'I','AAC':'N','ATA':'
              'GAA':'E','TGG':'W','GCA':'A','GTA':'V','GCC':'A','GTC':'V','GCG':'A','GTG':'V',
              'TTC':'F','GTT':'V','GCT':'A','TGA':'*','TTG':'L','TCC':'S','TCA':'S','TCT':'S'}
 
-
+# Input variables
 inputfile = 'tolC.fasta'
+posstartcodon = 64
+
+# Read gene sequence and translate
 genename=inputfile.split('.')[0]
 protsequencefilename=''.join([inputfile.split('.')[:-1], '-protein.', inputfile.split('.')[-1]])
 protsequencefile=open(protsequencefilename, 'w')
 nucsequence = open(inputfile, 'r').read().split('\n')[1].lower()
-posstartcodon = 64
 nucstart=posstartcodon-1 # -1 comes due to 0 indexing
+
 pos=nucstart
 protsequence=''
 while translation[nucsequence[pos:pos+3]]!='*':
     protsequence+=translation[nucsequence[pos:pos+3]]
     pos+=3
 protsequence=protsequence.capitalize()
-
+nucseqcount = list(range(nucstart, nucstart + 3 * len(protsequence) + 3, 3))
 posstopcodon = [i for i in range(64,len(nucsequence),3) if translation[nucsequence[i:i+3]]=='*'][0]
 
-tm_target=63 # Target mean tm temperature to set for all primers
+# Design Parameters
+tm_target=63 #  Target mean tm temperature (˚C) to set for all primers
+srange=3 # Sensitivity range for tm differences (˚C)
