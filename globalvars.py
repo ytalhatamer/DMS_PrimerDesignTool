d4f5c502dc8cbbd490e71b8b52d4fbe6ff42cc11
ytamer <yusuf.tamer@utsouthwestern.edu>
2020-09-23 11:35:36 -0500

    Dataframe is defined in import step.

--- globalvars.py
+++ globalvars.py
@@ -1,34 +1,60 @@
+import pandas as pd
+
 # Global codon translation table
-translation={'ACC':'T','ATG':'M','AAG':'K','AAA':'K','ATC':'I','AAC':'N','ATA':'I','AGG':'R',
-             'CCT':'P','CTC':'L','AGC':'S','ACA':'T','AGA':'R','CAT':'H','AAT':'N','ATT':'I',
-             'CTG':'L','CTA':'L','ACT':'T','CAC':'H','ACG':'T','CAA':'Q','AGT':'S','CAG':'Q',
-             'CCG':'P','CCC':'P','TAT':'Y','GGT':'G','TGT':'C','CGA':'R','CCA':'P','CGC':'R',
-             'GAT':'D','CGG':'R','CTT':'L','TGC':'C','GGG':'G','TAG':'*','GGA':'G','TAA':'*',
-             'GGC':'G','TAC':'Y','GAG':'E','TCG':'S','TTA':'L','TTT':'F','GAC':'D','CGT':'R',
-             'GAA':'E','TGG':'W','GCA':'A','GTA':'V','GCC':'A','GTC':'V','GCG':'A','GTG':'V',
-             'TTC':'F','GTT':'V','GCT':'A','TGA':'*','TTG':'L','TCC':'S','TCA':'S','TCT':'S'}
+translation = {'ACC': 'T', 'ATG': 'M', 'AAG': 'K', 'AAA': 'K', 'ATC': 'I',
+               'AAC': 'N', 'ATA': 'I', 'AGG': 'R', 'CCT': 'P', 'CTC': 'L',
+               'AGC': 'S', 'ACA': 'T', 'AGA': 'R', 'CAT': 'H', 'AAT': 'N',
+               'ATT': 'I', 'CTG': 'L', 'CTA': 'L', 'ACT': 'T', 'CAC': 'H',
+               'ACG': 'T', 'CAA': 'Q', 'AGT': 'S', 'CAG': 'Q', 'CCG': 'P',
+               'CCC': 'P', 'TAT': 'Y', 'GGT': 'G', 'TGT': 'C', 'CGA': 'R',
+               'CCA': 'P', 'CGC': 'R', 'GAT': 'D', 'CGG': 'R', 'CTT': 'L',
+               'TGC': 'C', 'GGG': 'G', 'TAG': '*', 'GGA': 'G', 'TAA': '*',
+               'GGC': 'G', 'TAC': 'Y', 'GAG': 'E', 'TCG': 'S', 'TTA': 'L',
+               'TTT': 'F', 'GAC': 'D', 'CGT': 'R', 'GAA': 'E', 'TGG': 'W',
+               'GCA': 'A', 'GTA': 'V', 'GCC': 'A', 'GTC': 'V', 'GCG': 'A',
+               'GTG': 'V', 'TTC': 'F', 'GTT': 'V', 'GCT': 'A', 'TGA': '*',
+               'TTG': 'L', 'TCC': 'S', 'TCA': 'S', 'TCT': 'S'}
 
 # Input variables
-inputfile = 'tolC.fasta'
-print('\n\nPosition example: aaacccATGacacactgtgtct --> POSITION=7')
-posstartcodon = 64
+inputfile = 'tolC_nuc.fasta'
+print('\n\nFor finding the CDS start position use start indexing with 1.\n'
+      'Position example: aaacccATGacacactgtgtct --> POSITION=7')
+# Before the CDS starts at least 60 nucleotide needs to be provided.
+# This is important for common primer design
+posstartcodon = 65
+print('Start codon position: {0}'.format(posstartcodon))
 
 # Read gene sequence and translate
-genename=inputfile.split('.')[0]
-protsequencefilename=''.join([inputfile.split('.')[0], '-protein.', inputfile.split('.')[-1]])
-protsequencefile=open(protsequencefilename, 'w')
+genename = inputfile.split('.')[0]
+protsequencefilename = ''.join([inputfile.split('.')[0], '-protein.',
+                                inputfile.split('.')[-1]])
+protsequencefile = open(protsequencefilename, 'w')
 nucsequence = open(inputfile, 'r').read().split('\n')[1].lower()
-nucstart=posstartcodon-1 # -1 comes due to 0 indexing
+nucstart = posstartcodon - 1  # -1 comes due to 0 indexing
 
-pos=nucstart
-protsequence=''
-while translation[nucsequence[pos:pos+3]]!='*':
-    protsequence+=translation[nucsequence[pos:pos+3]]
-    pos+=3
-protsequence=protsequence.capitalize()
+pos = nucstart
+protsequence = ''
+while translation[nucsequence[pos:pos + 3].upper()] != '*':
+    protsequence += translation[nucsequence[pos:pos + 3].upper()]
+    pos += 3
+
+posstopcodon = pos
+print('Protein Sequence : ', protsequence)
+print('Stop codon position: {0}'.format(posstopcodon))
+protsequence = protsequence.capitalize()
 nucseqcount = list(range(nucstart, nucstart + 3 * len(protsequence) + 3, 3))
-posstopcodon = [i for i in range(64,len(nucsequence),3) if translation[nucsequence[i:i+3]]=='*'][0]
 
 # Design Parameters
-tm_target=63 #  Target mean tm temperature (˚C) to set for all primers
-srange=3 # Sensitivity range for tm differences (˚C)
+tm_target = 63  # Target mean tm temperature (˚C) to set for all primers
+srange = 3  # Sensitivity range for tm differences (˚C)
+
+typedict = {'Residue Number': 'int64', 'Primer Name': 'object',
+            'Primer Sequence (Fwd)': 'object', 'Primer Sequence (Rev-Comp)': 'object',
+            'Tm': 'float64', 'GC Content (%)': 'float64', 'Primer Length': 'int64',
+            'GC Score': 'float64', 'Tm Score': 'float64', 'Length Score': 'int64',
+            'Wing Score': 'float64', 'Total Score': 'float64'}
+
+df = pd.DataFrame([], columns=['Residue Number', 'Primer Name', 'Primer Sequence (Fwd)',
+                               'Primer Sequence (Rev-Comp)', 'Tm', 'GC Content (%)',
+                               'Primer Length', 'GC Score', 'Tm Score',
+                               'Length Score', 'Wing Score', 'Total Score'])
